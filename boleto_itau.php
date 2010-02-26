9384fc12072b15fc0af7ad026b25d1bee82c9123
Alexandre Santos Work <alexandre@diariodecodiogs.info>
2010-02-26 14:32:18 -0300

    Upando projeto para GitHub

---
+++ boleto_itau.php
@@ -0,0 +1,118 @@
+﻿<?php
+// +----------------------------------------------------------------------+
+// | BoletoPhp - Versão Beta                                              |
+// +----------------------------------------------------------------------+
+// | Este arquivo está disponível sob a Licença GPL disponível pela Web   |
+// | em http://pt.wikipedia.org/wiki/GNU_General_Public_License           |
+// | Você deve ter recebido uma cópia da GNU Public License junto com     |
+// | esse pacote; se não, escreva para:                                   |
+// |                                                                      |
+// | Free Software Foundation, Inc.                                       |
+// | 59 Temple Place - Suite 330                                          |
+// | Boston, MA 02111-1307, USA.                                          |
+// +----------------------------------------------------------------------+
+
+// +----------------------------------------------------------------------+
+// | Originado do Projeto BBBoletoFree que tiveram colaborações de Daniel |
+// | William Schultz e Leandro Maniezo que por sua vez foi derivado do	  |
+// | PHPBoleto de João Prado Maia e Pablo Martins F. Costa				  |
+// | 																	  |
+// | Se vc quer colaborar, nos ajude a desenvolver p/ os demais bancos :-)|
+// | Acesse o site do Projeto BoletoPhp: www.boletophp.com.br             |
+// +----------------------------------------------------------------------+
+
+// +----------------------------------------------------------------------+
+// | Equipe Coordenação Projeto BoletoPhp: <boletophp@boletophp.com.br>   |
+// | Desenvolvimento Boleto Itaú: Glauber Portella		                  |
+// +----------------------------------------------------------------------+
+
+$nosso_numero = $_POST['nosso_numero'];
+$numero_documento = $_POST['numero_documento'];
+
+$sacado = $_POST['sacado'];
+$endereco = $_POST['endereco'];
+$cidade = $_POST['cidade'];
+$estado = $_POST['estado'];
+$CEP = $_POST['cep'];
+$matricula = $_POST['matricula'];
+$cpf = $_POST['cpf'];
+
+$data_vencimento = $_POST['data_vencimento'];
+$valor_cobrado = $_POST['valor_cobrado'];
+
+$demonstrativo1 = $_POST['demonstrativo1'];
+$demonstrativo2 = $_POST['taxa_boleto'];
+$demonstrativo3 = $_POST['demonstrativo3'];
+
+$descricao1 = $_POST['descricao1'];
+$descricao2 = $_POST['descricao2'];
+$descricao3 = $_POST['descricao3'];
+$descricao4 = $_POST['descricao4'];
+
+// ------------------------- DADOS DINÂMICOS DO SEU CLIENTE PARA A GERAÇÃO DO BOLETO (FIXO OU VIA GET) -------------------- //
+// Os valores abaixo podem ser colocados manualmente ou ajustados p/ formulário c/ POST, GET ou de BD (MySql,Postgre,etc)	//
+
+// DADOS DO BOLETO PARA O SEU CLIENTE
+$dias_de_prazo_para_pagamento = 5;
+if($demonstrativo2 == 'sim') {
+	$taxa_boleto = "3,00";
+}else{
+	$taxa_boleto = 0;
+}
+$data_venc = date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));  // Prazo de X dias OU informe data: "13/04/2006"; 
+$valor_cobrado = $valor_cobrado; // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
+$valor_cobrado = str_replace(",", ".",$valor_cobrado);
+$valor_boleto=number_format($valor_cobrado+$taxa_boleto, 2, ',', '');
+
+$dadosboleto["nosso_numero"] = $nosso_numero;  // Nosso numero - REGRA: Máximo de 8 caracteres!
+$dadosboleto["numero_documento"] = $numero_documento;	// Num do pedido ou nosso numero
+$dadosboleto["data_vencimento"] = $data_vencimento; // Data de Vencimento do Boleto - REGRA: Formato DD/MM/AAAA
+$dadosboleto["data_documento"] = date("d/m/Y"); // Data de emissão do Boleto
+$dadosboleto["data_processamento"] = date("d/m/Y"); // Data de processamento do boleto (opcional)
+$dadosboleto["valor_boleto"] = $valor_boleto; 	// Valor do Boleto - REGRA: Com vírgula e sempre com duas casas depois da virgula
+
+// DADOS DO SEU CLIENTE
+$dadosboleto["sacado"] = "$sacado - $matricula - <strong>CPF:</strong> $cpf";
+$dadosboleto["endereco1"] = $endereco;
+$dadosboleto["endereco2"] = "$cidade - $estado - CEP: $CEP";
+
+// INFORMACOES PARA O CLIENTE
+$dadosboleto["demonstrativo1"] = $demonstrativo1;
+if ($demonstrativo2 == 'sim') { $dadosboleto["demonstrativo2"] = "Taxa bancária - R$ ".$taxa_boleto;};
+$dadosboleto["demonstrativo3"] = $demonstrativo3;
+$dadosboleto["instrucoes1"] = "- $descricao1";
+$dadosboleto["instrucoes2"] = "- $descricao2";
+$dadosboleto["instrucoes3"] = "- $descricao3";
+$dadosboleto["instrucoes4"] = "&nbsp; $descricao4";
+
+// DADOS OPCIONAIS DE ACORDO COM O BANCO OU CLIENTE
+$dadosboleto["quantidade"] = "";
+$dadosboleto["valor_unitario"] = "";
+$dadosboleto["aceite"] = "";		
+$dadosboleto["uso_banco"] = ""; 	
+$dadosboleto["especie"] = "R$";
+$dadosboleto["especie_doc"] = "";
+
+
+// ---------------------- DADOS FIXOS DE CONFIGURAÇÃO DO SEU BOLETO --------------- //
+
+
+// DADOS DA SUA CONTA - ITAÚ
+$dadosboleto["agencia"] = "6476"; // Num da agencia, sem digito
+$dadosboleto["conta"] = "07274";	// Num da conta, sem digito
+$dadosboleto["conta_dv"] = "6"; 	// Digito do Num da conta
+
+// DADOS PERSONALIZADOS - ITAÚ
+$dadosboleto["carteira"] = "175";  // Código da Carteira
+
+// SEUS DADOS
+$dadosboleto["identificacao"] = "AWU - American World University";
+$dadosboleto["cpf_cnpj"] = "31036114/0001-15";
+$dadosboleto["endereco"] = "Rua Monsenhor Jerônimo 736";
+$dadosboleto["cidade_uf"] = "Rio de Janeiro - RJ - Brasil";
+$dadosboleto["cedente"] = "GPS E P DESPORTIVAS";
+
+// NÃO ALTERAR!
+include("include/funcoes_itau.php"); 
+include("include/layout_itau.php");
+?>
---
+++ index.php
@@ -0,0 +1,179 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Gerador de Boleto AWU</title>
+<style type="text/css">
+<!--
+* {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 12px;
+}
+input {
+	width: 250px;
+}
+a.tooltip{
+	position:relative;
+	font:12px arial, verdana, helvetica, sans-serif;
+	padding:0;
+	color:#0000FF;
+	text-decoration:none;
+	border-bottom:2px dotted #039;
+	cursor:help;
+	z-index:50;
+	font-weight:bold;
+}
+       a.tooltip:hover{
+            background:transparent;
+            z-index:55;
+       }
+       a.tooltip span{
+            display:none;
+       }
+       a.tooltip:hover span{
+	display:block;
+	position:absolute;
+	width:350px;
+	top:2em;
+	text-align:justify;
+	left:-7em;
+	font: 12px arial, verdana, helvetica, sans-serif;
+	padding:5px 10px;
+	border:1px solid #999;
+	color:#333333;
+	background-color: #FFFFFF;
+      }
+-->
+</style>
+<link rel="shortcut icon" href="favicon.png" />
+<script src="SpryAssets/SpryValidationTextField.js" type="text/javascript"></script>
+<link href="SpryAssets/SpryValidationTextField.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+<form id="form1" name="form1" method="post" action="boleto_itau.php">
+<table width="647" border="0" align="center" cellpadding="5" cellspacing="0">
+  <tr>
+    <td width="242" align="right">Nosso Número <br />
+    (Máximo de 8 Caracteres, Ex. 12345678)</td>
+    <td width="389" align="left"><span id="sprytextfield3">
+      <input name="nosso_numero" type="text" id="nosso_numero" maxlength="8" />
+      <span class="textfieldRequiredMsg">Preencha o campo.</span></span></td>
+  </tr>
+  <tr>
+    <td align="right">Número do Documento<br />
+    Número do pedido do nosso número</td>
+    <td align="left"><span id="sprytextfield4">
+      <input name="numero_documento" type="text" id="numero_documento" maxlength="4" />
+      <span class="textfieldRequiredMsg">Preencha o campo.</span></span></td>
+  </tr>
+  <tr>
+    <td colspan="2" align="right"><hr /></td>
+    </tr>
+  <tr>
+    <td align="right">Sacado</td>
+    <td align="left"><input type="text" name="sacado" id="sacado" /></td>
+  </tr>
+  <tr>
+    <td align="right">Endereço Sacado</td>
+    <td align="left"><input type="text" name="endereco" id="endereco" /></td>
+  </tr>
+  <tr>
+    <td align="right">Cidade Sacado</td>
+    <td align="left"><input type="text" name="cidade" id="cidade" /></td>
+  </tr>
+  <tr>
+    <td align="right">Estado Sacado</td>
+    <td align="left"><input type="text" name="estado" id="estado" /></td>
+  </tr>
+  <tr>
+    <td align="right">CEP Sacado</td>
+    <td align="left"><input type="text" name="cep" id="cep" /></td>
+  </tr>
+  <tr>
+    <td align="right">Matrícula</td>
+    <td align="left"><input type="text" name="matricula" id="matricula" /></td>
+  </tr>
+  <tr>
+    <td align="right">CPF</td>
+    <td align="left"><input type="text" name="cpf" id="cpf" /></td>
+  </tr>
+  <tr>
+    <td colspan="2" align="right"><hr /></td>
+    </tr>
+  <tr>
+    <td align="right">Data Vencimento<br />
+      (Ex.DD/MM/AA) </td>
+    <td align="left"><span id="sprytextfield1">
+    <input type="text" name="data_vencimento" id="data_vencimento" />
+    <span class="textfieldRequiredMsg">Valor Inválido</span><span class="textfieldInvalidFormatMsg"><br />Formato de data Inv?do.</span></span></td>
+  </tr>
+  <tr>
+    <td align="right">Valor Cobrado<br />
+      (Ex. 125,00)</td>
+    <td align="left"><span id="sprytextfield2">
+    <input type="text" name="valor_cobrado" id="valor_cobrado" />
+    <span class="textfieldRequiredMsg">Valor Inválido.</span><span class="textfieldInvalidFormatMsg"><br />Formato de mo? inv?da.</span></span></td>
+  </tr>
+  <tr>
+    <td colspan="2" align="right"><hr /></td>
+    </tr>
+  <tr>
+    <td align="right">Demonstrativo 1</td>
+    <td align="left"><input type="text" name="demonstrativo1" id="demonstrativo1" value="Pagamento Referente a Livros da AWU/LAD/USA" /></td>
+  </tr>
+  <tr>
+    <td align="right">Demonstrativo 2 (Taxa Boleto)</td>
+    <td align="left">
+      <select name="taxa_boleto" id="taxa_boleto">
+        <option value="sim" selected="selected">Sim</option>
+        <option value="nao">Não</option>
+      </select>
+      </td>
+  </tr>
+  <tr>
+    <td align="right">Demonstrativo 3</td>
+    <td align="left"><input type="text" name="demonstrativo3" id="demonstrativo3" value="AWU/LAD/USA - http://www.awu.com.br" /></td>
+  </tr>
+  <tr>
+    <td colspan="2" align="right"><hr /></td>
+    </tr>
+  <tr>
+    <td align="right">Descrição 1:</td>
+    <td align="left"><input type="text" name="descricao1" id="descricao1" value="Após vencimento multa de 2% a.m + juros 0,03% a.d" /></td>
+  </tr>
+  <tr>
+    <td align="right">Descrição 2:</td>
+    <td align="left"><input type="text" name="descricao2" id="descricao2" value="Em caso de dúvidas entre em contato conosco: +55 (21) 3276-3744 ou contato@awu.com.br" /></td>
+  </tr>
+  <tr>
+    <td align="right">Descrição 3:</td>
+    <td align="left"><input type="text" name="descricao3" id="descricao3" />
+     <a href="#" class="tooltip"><img src="imagens/interrogacao.png" border="0" alt="Tool Tip" />
+     <span>Coloque em Descrição 4 o nome do produto com o valor, exemplo:<br />
+     		<strong>Descrição 1</strong>: Livro Metodologia da Pesquisa Científica 1 - R$ 50,00 / Livro Didádica e Andragogia - R$ 40,00/<br />
+      </span></a>
+    </td>
+  </tr>
+    <tr>
+    <td align="right">Descrição 4:</td>
+    <td align="left"><input type="text" name="descricao4" id="descricao4" />
+   </td>
+  </tr>
+  <tr>
+    <td colspan="2" align="center"><input type="submit" name="button" id="button" value="Gerar Boleto" /></td>
+    </tr>
+</table>
+</form>
+ 
+<script type="text/javascript">
+<!--
+var sprytextfield1 = new Spry.Widget.ValidationTextField("sprytextfield1", "date", {format:"dd/mm/yyyy", validateOn:["blur", "change"]});
+var sprytextfield2 = new Spry.Widget.ValidationTextField("sprytextfield2", "currency", {format:"dot_comma", validateOn:["blur", "change"]});
+var sprytextfield3 = new Spry.Widget.ValidationTextField("sprytextfield3", "none", {validateOn:["blur", "change"]});
+var sprytextfield4 = new Spry.Widget.ValidationTextField("sprytextfield4", "none", {validateOn:["blur", "change"]});
+//-->
+</script>
+</body>
+</html>
